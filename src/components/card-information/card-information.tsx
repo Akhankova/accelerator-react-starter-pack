ee9cb07c0654e0e50146a1e4e87b01e19e9fe5fb
Alina Akhankova <a.akhankova@gmail.com>
2022-03-18 00:06:07 +0100

    Downloaded comments from server

--- src/components/card-information/card-information.tsx
+++ src/components/card-information/card-information.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
-import { SmallCard } from '../../types/cards';
+import { SmallCard, Comments } from '../../types/cards';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
@@ -12,10 +12,20 @@ import { ModalCardAdd } from '../modal-cart-add/modal-cart-add';
 function CardInformation(): JSX.Element {
   const numberCurrentCardId = useParams<{ id?: string }>().id;
   const [card, setCard] = useState<SmallCard>();
+  const [comments, setComments] = useState<Comments>();
+  // eslint-disable-next-line no-console
+  console.log(comments);
   const history = useHistory();
   useEffect(() => {
     api.get(`https://accelerator-guitar-shop-api-v1.glitch.me/guitars/${numberCurrentCardId}`)
       .then((response) => setCard(response.data))
+      // eslint-disable-next-line no-console
+      .catch(() => console.log('Произошла ошибка при загрузке. Повторите попытку'));
+  }, [history, numberCurrentCardId]);
+
+  useEffect(() => {
+    api.get(`https://accelerator-guitar-shop-api-v1.glitch.me/guitars/${numberCurrentCardId}/comments`)
+      .then((response) => setComments(response.data))
       // eslint-disable-next-line no-console
       .catch(() => console.log('Произошла ошибка при загрузке. Повторите попытку'));
   }, [history, numberCurrentCardId]);
@@ -138,13 +148,48 @@ function CardInformation(): JSX.Element {
                 <a className="button button--red button--big product-container__button" onClick={onBookingBtnClick}>Добавить в корзину</a>
               </div>
             </div>
+            <section className="reviews">
+              <h3 className="reviews__title title title--bigger">Отзывы</h3><a className="button button--red-border button--big reviews__submit-button" href="/">Оставить отзыв</a>
+              {comments?.slice().map((comment) => (
+                <div className="review" key={comment.id}>
+                  <div className="review__wrapper">
+                    <h4 className="review__title review__title--author title title--lesser">{comment.userName}</h4><span className="review__date">{comment.createAt}</span>
+                  </div>
+                  <div className="rate review__rating-panel" aria-hidden="true"><span className="visually-hidden">Рейтинг:</span>
+                    <svg width="16" height="16" aria-hidden="true">
+                      <use xlinkHref={comment.rating >= 1 ? '#icon-full-star' : '#icon-star'}></use>
+                    </svg>
+                    <svg width="16" height="16" aria-hidden="true">
+                      <use xlinkHref={comment.rating >= 2 ? '#icon-full-star' : '#icon-star'}></use>
+                    </svg>
+                    <svg width="16" height="16" aria-hidden="true">
+                      <use xlinkHref={comment.rating >= 3 ? '#icon-full-star' : '#icon-star'}></use>
+                    </svg>
+                    <svg width="16" height="16" aria-hidden="true">
+                      <use xlinkHref={comment.rating >= 4 ? '#icon-full-star' : '#icon-star'}></use>
+                    </svg>
+                    <svg width="16" height="16" aria-hidden="true">
+                      <use xlinkHref={comment.rating >= 5 ? '#icon-full-star' : '#icon-star'}></use>
+                    </svg><span className="rate__count"></span><span className="rate__message"></span>
+                  </div>
+                  <h4 className="review__title title title--lesser">Достоинства:</h4>
+                  <p className="review__value">{comment.advantage}</p>
+                  <h4 className="review__title title title--lesser">Недостатки:</h4>
+                  <p className="review__value">{comment.disadvantage}</p>
+                  <h4 className="review__title title title--lesser">Комментарий:</h4>
+                  <p className="review__value">{comment.comment}</p>
+                </div>
+              ))}
+
+              <button className="button button--medium reviews__more-button">Показать еще отзывы</button><a className="button button--up button--red-border button--big reviews__up-button" href="#header">Наверх</a>
+            </section>
           </div>
         </main>
         {!isBookingModalOpened ? null : <ModalCardAdd
           isVisible={isBookingModalOpened}
           onClose={() => setIsBookingModalOpened(false)}
           // eslint-disable-next-line react/jsx-closing-bracket-location
-          card={card}/>}
+          card={card} />}
 
         <Footer />
       </div>
--- src/types/cards.ts
+++ src/types/cards.ts
@@ -11,3 +11,16 @@ export type SmallCard = {
 };
 
 export type SmallCards = SmallCard[];
+
+export type Comment = {
+    id: string,
+    userName: string,
+    advantage: string,
+    disadvantage: string,
+    comment: string,
+    rating: number,
+    createAt: string,
+    guitarId: number
+};
+
+export type Comments = Comment[];
