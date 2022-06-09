f9907d9e8d0e97010ebded36383692b1b62d8351
Alina Akhankova <a.akhankova@gmail.com>
2022-06-09 18:21:12 +0200

    Added new dayjs format and ru language

--- src/components/card-information/card-information.tsx
+++ src/components/card-information/card-information.tsx
@@ -226,7 +226,7 @@ function CardInformation(): JSX.Element {
                 {commentsLoading ? comments?.slice().splice(0, visiblyFilmCount).map((comment) => (
                   <div className="review" key={comment.id}>
                     <div className="review__wrapper">
-                      <h4 className="review__title review__title--author title title--lesser">{comment.userName}</h4><span className="review__date">{dayjs(comment.createAt).fromNow()}</span>
+                      <h4 className="review__title review__title--author title title--lesser">{comment.userName}</h4><span className="review__date">{dayjs(comment.createAt).locale('ru').format('DD MMMM')}</span>
                     </div>
                     <div className="rate review__rating-panel" aria-hidden="true"><span className="visually-hidden">Рейтинг:</span>
                       <svg width="16" height="16" aria-hidden="true">
